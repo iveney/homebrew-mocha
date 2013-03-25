05baab308abb5ac3655b87f64ed19f4ca1bd42c9
Zigang Xiao <iveney@gmail.com>
2013-03-24 19:12:02 -0500

    Add formula: realpath

---
+++ realpath.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Realpath < Formula
+  version "0.1"
+  homepage 'https://github.com/harto/realpath-osxb'
+  url 'https://github.com/iveney/realpath-osx.git'
+  head 'https://github.com/iveney/realpath-osx.git'
+  sha1 '4d4e0b1bc7a491e69270a8ef7c809eb9c8874f00'
+
+  def install
+    system "make"
+    bin.install "realpath"
+  end
+
+
+end
