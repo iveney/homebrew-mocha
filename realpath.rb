1f46103f8f342eeb61bcbaaab32cad1fbf6a62bc
Zigang Xiao <iveney@gmail.com>
2014-05-01 19:49:39 -0500

    Fix bug

--- realpath.rb
+++ realpath.rb
@@ -5,7 +5,7 @@ require 'formula'
 
 class Realpath < Formula
   version "0.1"
-  homepage 'https://github.com/harto/realpath-osxb'
+  homepage 'https://github.com/harto/realpath-osx'
   url 'https://github.com/iveney/realpath-osx.git'
   head 'https://github.com/iveney/realpath-osx.git'
   sha1 '4d4e0b1bc7a491e69270a8ef7c809eb9c8874f00'
